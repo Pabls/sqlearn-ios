eaf86129420ab6389c685eb309ef693aeda6e8a7
Semin Pavel <pabls@yandex.ru>
2020-01-25 19:36:25 +0300

    init

---
+++ Sqlearn/Sqlearn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sqlearn
+//
+//  Created by Pavel Semin on 25.01.2020.
+//  Copyright © 2020 Pavel Semin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
